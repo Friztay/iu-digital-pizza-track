9797c55f397e3a000b53927aca2662a58d0095fd
Friztay <secopa8697@gmail.com>
2026-03-15 20:34:09 -0500

    Implementa centralizador de ejecucion

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import services.MenuService;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuService menuService = new MenuService();
+        menuService.inicializarGestorPedidos();
+    }
+}
